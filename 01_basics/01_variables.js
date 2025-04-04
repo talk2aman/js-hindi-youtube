b0e20fc91926823175e823489e771756876166f6
talk2aman <amanpreetsin@gmail.com>
2025-04-04 12:09:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 8738;
+let accountEmail = "aman@yahoo.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
