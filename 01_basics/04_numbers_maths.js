1bb5c4e2a46aea0f037d7b56941bc7c879038501
talk2aman <amanpreetsin@gmail.com>
2025-04-09 06:46:40 +0000

    maths added

--- 01_basics/04_numbers_maths.js
+++ 01_basics/04_numbers_maths.js
@@ -14,13 +14,23 @@ const newNumber = 1000000
 console.log(newNumber.toLocaleString('eng-IN'))
 
 console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
-console.log(Math.max(4, 6, 9, 10));
-console.log(Math.min(4, 6, 9, 10));
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.max(4, 6, 9, 10))
+console.log(Math.min(4, 6, 9, 10))
 
- console.log(Math.random());
+ console.log(Math.random())
  console.log(Math.random() * 10)
+ console.log(Math.random()*10 + 1)
+ console.log(Math.floor(Math.random()*10) + 1)
+
+ const min = 10
+ const max = 20
+
+ Math.floor(Math.random() * (max - min + 1) + min)
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+ 
+ 
  
 
 
